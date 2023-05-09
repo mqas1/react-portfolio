9a3ea1da21ec83f2464c92f5a0c67f8123409203
mqas1 <114903874+mqas1@users.noreply.github.com>
2023-05-09 23:45:15 +1000

    adds LiveLocal

--- react-portfolio/src/projects.js
+++ react-portfolio/src/projects.js
@@ -3,7 +3,8 @@ import openChatBox from './assets/images/OpenChatBox.jpeg';
 import JATE from './assets/images/JATE.jpeg';
 import noteTaker from './assets/images/NoteTaker.jpeg';
 import weatherDashboard from './assets/images/WeatherDashboard.jpeg';
-import workDayScheduler from './assets/images/WorkDayScheduler.jpeg';
+
+import liveLocal from './assets/images/eventsScreenshot.jpeg';
 
 const projects = [
   {
@@ -23,8 +24,16 @@ const projects = [
     github: 'https://github.com/mqas1/open-chat-box',
   },
   {
-    title: 'J.A.T.E',
+    title: 'LiveLocal',
     id: 3,
+    technologies: 'MERN Stack',
+    imgage: liveLocal,
+    link: 'https://livelocal.herokuapp.com/',
+    github: 'https://github.com/mqas1/livelocal'
+  },
+  {
+    title: 'J.A.T.E',
+    id: 4,
     technologies: 'Full Stack PWA',
     image: JATE,
     link: 'https://mqas1-text-editor.herokuapp.com/',
@@ -32,7 +41,7 @@ const projects = [
   },
   {
     title: 'Note Taker',
-    id: 4,
+    id: 5,
     technologies: 'Full Stack',
     image: noteTaker,
     link: 'https://note-taker-mqas1.herokuapp.com/',
@@ -40,19 +49,11 @@ const projects = [
   },
   {
     title: 'Weather Dashboard',
-    id: 5,
+    id: 6,
     technologies: 'HTML/CSS/JavaScript',
     image: weatherDashboard,
     link: 'https://mqas1.github.io/weather-dashboard/',
     github: 'https://github.com/mqas1/weather-dashboard',
-  },
-  {
-    title: 'Work Day Scheduler',
-    id: 6,
-    technologies: 'HTML/CSS/JavaScript',
-    image: workDayScheduler,
-    link: 'https://mqas1.github.io/work-day-scheduler/',
-    github: 'https://github.com/mqas1/work-day-scheduler',
   },
 ];
 
